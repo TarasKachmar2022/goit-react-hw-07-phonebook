da3d6481973e415f29bea378eba075862b7eb920
Taras Kachmar <kachmar.taras@gmail.com>
2023-06-09 22:51:37 +0300

    change images url

--- src/styled/globalStyle.js
+++ src/styled/globalStyle.js
@@ -9,7 +9,7 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: url('https://pixabay.com/get/gf3141abc167f8403eefda61544e7db1d982ba6d585ceb4419580f9e40ffc5c95367da1e2b2b16e0b4ef08a91940cb1ec5fe7a82b5f04f0eae8d535c1d1c2c586_1280.jpg') 0 0 / cover no-repeat;
+  background: url('https://pixabay.com/get/g54be0da25854975431e59cb55609c112235384ef09803972a59a450e0039749c9ec06cdc040b158118c59a57645d3e1a190107080a2f8692ac55529d4475fedd_1280.jpg') 0 0 / cover no-repeat;
   background-attachment: fixed;
 }
 
